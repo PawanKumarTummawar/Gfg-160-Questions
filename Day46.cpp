e65b393923f90eb65963f1464f96bf83ebf66e12
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-01-08 13:08:18 +0530

    Day-46

---
+++ Day46.cpp
@@ -0,0 +1,18 @@
+// Union of Arrays with Duplicates
+
+// int findUnion(vector<int>& a, vector<int>& b) {
+//         unordered_map<int, int> mpp;
+//         int ans = 0;
+//         for(int x: a) {
+//             if(!mpp[x]) {
+//                 mpp[x] = 1;
+//                 ans++;
+//             }
+//         }
+//         for(int x: b) {
+//             if(!mpp[x]){
+//                 mpp[x] = 1;
+//                 ans++;
+//             }
+//         }
+//         return ans;
