a50a72938959c0433603048e40c117c59d6cd9a4
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-01-14 13:50:44 +0530

    day-51

---
+++ Day51.cpp
@@ -0,0 +1,29 @@
+// Count all triplets with given sum in sorted array
+
+int countTriplets(vector<int> &arr, int target)
+{
+    int n = arr.size(), count = 0, sum = 0;
+    for (int i = 0; i <= n - 3; i++)
+    {
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            sum = arr[i] + arr[j] + arr[k];
+            if (sum < target)
+                j++;
+            else if (sum > target)
+                k--;
+            else if (sum == target)
+            {
+                count++;
+                int temp = j + 1;
+                while (temp < k && arr[temp] == arr[temp - 1])
+                {
+                    count++;
+                    temp++;
+                }
+                k--;
+            }
+        }
+    }
+    return count;
