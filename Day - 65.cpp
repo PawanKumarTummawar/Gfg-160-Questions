311a181a30dbc7d27cb279058c9fe8c132bf3ea3
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-01-27 22:40:48 +0530

    Day - 65

---
+++ Day - 65.cpp
@@ -0,0 +1,15 @@
+Reverse a linked list
+
+    Node *
+    reverseList(struct Node *head)
+{
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp != NULL)
+    {
+        Node *front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
