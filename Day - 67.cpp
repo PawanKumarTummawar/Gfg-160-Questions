5cd14446bc777b581f83f4e13d703359e591d5b9
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-01-29 23:08:41 +0530

    Merge two sorted linked lists

---
+++ Day - 67.cpp
@@ -0,0 +1,31 @@
+Merge two sorted linked lists
+
+    Node *
+    sortedMerge(Node *head1, Node *head2)
+{
+
+    Node *t1 = head1;
+    Node *t2 = head2;
+    Node *dummyNode = new Node(-1);
+    Node *temp = dummyNode;
+
+    while (t1 != NULL && t2 != NULL)
+    {
+        if (t1->data < t2->data)
+        {
+            temp->next = t1;
+            temp = t1;
+            t1 = t1->next;
+        }
+        else
+        {
+            temp->next = t2;
+            temp = t2;
+            t2 = t2->next;
+        }
+    }
+    if (t1)
+        temp->next = t1;
+    else
+        temp->next = t2;
+    return dummyNode->next;
