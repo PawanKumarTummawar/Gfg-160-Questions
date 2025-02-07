27ff9b43cb01e5bdfc5c16313b4bb762e58ca33d
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-02-07 23:27:10 +0530

    Implement Pow

--- Day - 76.cpp
+++ Day - 76.cpp
@@ -1,19 +1,16 @@
 Implement Pow
 
     double
-    power(double b, int e)
-{
-    if (e == 0)
-        return 1;
-    if (e < 0)
-        return 1 / power(b, -e);
+    power(double b, int e) if (e == 0) return 1;
+if (e < 0)
+    return 1 / power(b, -e);
 
-    double tempans = power(b, e / 2);
-    if (e % 2 == 0)
-    {
-        return tempans * tempans;
-    }
-    else
-    {
-        return b * tempans * tempans;
-    }
+double tempans = power(b, e / 2);
+if (e % 2 == 0)
+{
+    return tempans * tempans;
+}
+else
+{
+    return b * tempans * tempans;
+}
