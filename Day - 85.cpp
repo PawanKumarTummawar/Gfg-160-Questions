3448635c83b610b07181b72fb5a56cadc93c21a0
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-02-16 23:24:20 +0530

    Inorder Traversal

---
+++ Day - 85.cpp
@@ -0,0 +1,19 @@
+Inorder Traversal
+
+    void
+    inorder(Node *root, vector<int> &ans)
+{
+    if (!root)
+        return;
+
+    inorder(root->left, ans);
+    ans.push_back(root->data);
+    inorder(root->right, ans);
+}
+
+vector<int> inOrder(Node *root)
+{
+    vector<int> ans;
+    inorder(root, ans);
+    return ans;
+}
