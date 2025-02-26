dc06d5413271e0a50c006749157cda6e85d02c74
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-02-26 23:13:53 +0530

    Day - 95

---
+++ Day - 95.cpp
@@ -0,0 +1,31 @@
+k largest elements
+
+    vector<int>
+    kLargest(vector<int> &arr, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int i = 0; i < k; ++i)
+    {
+        pq.push(arr[i]);
+    }
+
+    for (int i = k; i < arr.size(); ++i)
+    {
+        if (arr[i] > pq.top())
+        {
+            pq.pop();
+            pq.push(arr[i]);
+        }
+    }
+
+    vector<int> res;
+    while (!pq.empty())
+    {
+        res.push_back(pq.top());
+        pq.pop();
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+}
