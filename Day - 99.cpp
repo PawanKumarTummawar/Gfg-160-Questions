9c32bbe6351cae01355b51f659620254549c0a99
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-03-02 23:04:39 +0530

    Parenthesis Checker

---
+++ Day - 99.cpp
@@ -0,0 +1,25 @@
+Parenthesis Checker
+
+    bool
+    isBalanced(string &s)
+{
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if (!st.empty() && ((st.top() == '(' && s[i] == ')') || (st.top() == '[' && s[i] == ']') || (st.top() == '{' && s[i] == '}')))
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
