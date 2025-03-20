95551d417dcf33c37cf442dab4329036b02a33fa
Pawankumar Tummawar <pawantummawar333@gmail.com>
2025-03-20 23:34:29 +0530

    Ways to Reach the n'th Stair

---
+++ Day - 117.cpp
@@ -0,0 +1,21 @@
+Ways to Reach the n'th Stair
+
+    int
+    solve(int n)
+{
+    int prev1 = 1;
+    int prev2 = 1;
+    int curr;
+    if (n <= 1)
+        return n;
+    for (int i = 2; i <= n; i++)
+    {
+        curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    return prev2;
+}
+int countWays(int n)
+{
+    return solve(n);
